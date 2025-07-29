d249a2dbb7076fce2b84f753e9fec79de0b192ab
Chigoziem <chigoziemwell@gmail.com>
2025-07-29 14:44:50 +0100

    worked on the sign in

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -19,6 +19,20 @@ const authConfig = {
       return !!auth?.user;
     },
   },
+  async signIn({ user, account, profile }) {
+    try {
+      const existingGuest = await getGuest(user.email);
+      if (!existingGuest) {
+        await createGuest({
+          email: user.email,
+          fullName: user.name,
+        });
+      }
+      return true;
+    } catch {
+      return false;
+    }
+  },
   async session({ session, user }) {
     const guest = await getGuest(session.user.email);
     session.user.guestId = guest[0]?.id || null; // Ensure guestId is set or null
